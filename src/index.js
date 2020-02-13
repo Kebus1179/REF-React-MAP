41d74bd4769fbfb557f3046d12882fc115f0b9be
Kebus1179 <61005347+Kebus1179@users.noreply.github.com>
2020-02-13 14:19:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+const myLists = ['A', 'B', 'C', 'D', 'D'];   
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(  <App myLists={myLists} />,  , rootElement);
+
