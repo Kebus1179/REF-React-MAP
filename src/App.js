b85276460670f90c17484b7216e318a5bd9cc56c
Kebus1179 <61005347+Kebus1179@users.noreply.github.com>
2020-02-13 14:18:32 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';   
+import ReactDOM from 'react-dom';   
+  
+function NameList(props) {  
+  const myLists = props.myLists;  
+  const listItems = myLists.map((myList) =>  
+    <li>{myList}</li>  
+  );  
+  return (  
+    <div>  
+          <h2>React Map Example</h2>  
+              <ul>{listItems}</ul>  
+    </div>  
+  );  
+}  
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+export default App;  
